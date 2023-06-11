ee8d6608b0312c36091818e6123c773684b3eca9
haritha mohan <harita.rithu@gmail.com>
2023-06-11 19:27:51 +0100

    modified API flow

--- cypress/e2e/DOMO_API_Flow.cy.js
+++ cypress/e2e/DOMO_API_Flow.cy.js
@@ -1,9 +1,10 @@
 import { validate } from "../support/util";
 
+// Validate API test for Domo
 
 describe("api test", () => {
     let isComment = false;
-    it.only("api testing", () => {
+    it ("API testing", () => {
         
         const expectedResponse = {
             token_type: "Bearer",
@@ -29,6 +30,39 @@ describe("api test", () => {
         const responseBody = response.body;
         validate(responseBody.data, expectedResponse);
       });
+
+      //Validation for Incorrect request body API 
+
+      cy.request({
+        method: "POST",
+        url: "https://api.signableapi.com/login",
+        body: {
+          email: "testtt",
+          password: "Password1",
+        },
+        failOnStatusCode: false, // Include this line to prevent Cypress from failing on non-2xx status codes
+      }).then((response) => {
+    
+        expect(response.status).to.equal(400); // Assert the response status code as 400 for incorrect request body
+        
     });
+
+      //Validation for Incorrect API Login URL 
+
+          cy.request({
+            method: "POST",
+            url: "https://api.signableapi.com/log",
+            body: {
+              email: "testtt",
+              password: "Password1",
+            },
+            failOnStatusCode: false, // Include this line to prevent Cypress from failing on non-2xx status codes
+          }).then((response) => {
+        
+            expect(response.status).to.equal(404); // Assert the response status code as 400 for incorrect request body
+            
+        });
 })
+})
+
   
